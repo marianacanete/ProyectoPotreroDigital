8e9611a783a8ffcd132b8282e5353876e80e17a1
Mar <marianacanete1@gmail.com>
2020-11-23 11:39:39 -0300

    cambios

--- js/Index.js
+++ js/Index.js
@@ -102,3 +102,66 @@
 		
 
 };
+
+/*MOSTRAR DIV OCULTO*/
+
+	function mostrarDivTarjeta () {
+
+		var tarjeta = document.getElementById("tarjetaOculto");
+		var rapi = document.getElementById("rapiOculto");
+		var conv = document.getElementById("convenirOculto");
+
+		tarjeta.style.display = 'inline-block';
+
+		if (rapi.style.display == 'inline-block' || conv.style.display == 'inline-block') {
+			rapi.style.display = 'none';
+			conv.style.display = 'none';
+		};
+};
+
+	function mostrarDivRapi () {
+		
+		var tarjeta = document.getElementById("tarjetaOculto");
+		var rapi = document.getElementById("rapiOculto");
+		var conv = document.getElementById("convenirOculto");
+
+		rapi.style.display = 'inline-block';
+
+		if (tarjeta.style.display == 'inline-block' || conv.style.display == 'inline-block') {
+			tarjeta.style.display = 'none';
+			conv.style.display = 'none';
+		};
+};
+
+
+	function mostrarDivConv() {
+	
+		var tarjeta = document.getElementById("tarjetaOculto");
+		var rapi = document.getElementById("rapiOculto");
+		var conv = document.getElementById("convenirOculto");
+
+		conv.style.display = 'inline-block';
+
+		if (rapi.style.display == 'inline-block' || tarjeta.style.display == 'inline-block') {
+			rapi.style.display = 'none';
+			tarjeta.style.display = 'none';
+		};
+};
+
+/*MOSTRAR DIV OCULTO*/
+
+	function finalizaCompra () {
+
+		var tarjeta = document.getElementById("tarjetaOculto");
+		var rapi = document.getElementById("rapiOculto");
+		var conv = document.getElementById("convenirOculto");
+
+
+		if ( rapi.style.display == 'inline-block' || tarjeta.style.display == 'inline-block' || conv.style.display == 'inline-block' ) {
+			alert ("Gracias por tu compra! te enviaremos un e-mail con los datos de tu orden");
+			window.location = 'potreroDigital_proyecto_grupo4_Index.html';	
+		} else {
+			alert ("Seleccione un medio de pago");
+		};
+
+	};
